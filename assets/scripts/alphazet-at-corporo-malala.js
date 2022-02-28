f11a12df467a6a59422cc7b4c1b34f31cabb3398
Madam Ella <madamellamalala@gmail.com>
2022-02-28 23:52:53 +0200

    Version 1.0.1
    - Added english translation of words to find.
    - Added a contribution link to google's api: https://translate.google.com/contribute.

--- assets/scripts/alphazet-at-corporo-malala.js
+++ assets/scripts/alphazet-at-corporo-malala.js
@@ -15,8 +15,11 @@ var textOffset = 0,
 
 function animaterectangular() {
   requestAnimationFrame(animaterectangular);
-  svgTextPathAfrikaans.setAttributeNS(null, "startOffset", textOffset + "%");
-  svgTextPathLingala.setAttributeNS(null, "startOffset", textOffset + "%");
+  
+  
+	svgTextPathFula.setAttributeNS(null, "startOffset", textOffset + "%");
+	svgTextPathAfrikaans.setAttributeNS(null, "startOffset", textOffset + "%");
+	svgTextPathLingala.setAttributeNS(null, "startOffset", textOffset + "%");
   if (textOffset >= 50) {
     textOffset = 0;
   }
@@ -31,8 +34,18 @@ animaterectangular();
 var defaultGameLanguage = document.querySelector(".js-body").getAttribute("gameLanguageAttribute"),
 	globalGameLanguage = "";
 
-getGameLanguage();
+setup()
 
+function setup() {
+	getGameLanguage();
+	focusContainer();
+}
+
+function focusContainer() {
+	var testTag = document.querySelector(".website");
+
+	testTag.focus();
+}
 function setGameLanguage(lang) {
 	const gameLanguageCookie = "gameLanguage=" + lang;
 	document.cookie = gameLanguageCookie + ";path=/";
@@ -55,44 +68,71 @@ function getGameLanguage() {
 /*== [GAME: HANGMAN -- Alphazet @ Corporo Malala] ==*/
       let game = null;
       const MAX_FAULTS = 9;
-      const wordList_afrikaans = [
-		'Aangenaame kennis',
-		'Asseblief',
-		'Dankie',
-		'Ek is jammer',
-		'Ek weet nie',
-		'Goed dankie, en met jou?',
-        'Goeie more',
-        'Goeie middag',
-        'Goeie nag',
-		'Ja',
-		'Jy is welkom',
-        'Hallo',
-		'Hoe laat is dit?',
-		'Hoe gaan dit met jou?',
-		'Hoeveel kos dit?',
-		'Nee',
-		'Sien jou later',
-		'Totsiens',
-		'Verskoon my',
-		'Waar is die badkamer',
-		'Wat is jou naam',
-		"Wag 'n bietjie",
-      ];
-      const wordList_lingala = [
-		'Mbote',
-		'Boni yo, ndeko?',
-		'Kombo na yo nini?',
-		'Matondi mingi',
-		'Mbotama elamu',
-		'Mbongo eza wapi?',
-		'Milolo! Milolo! Milolo!',
-		'Nasengi yo bolimbisi',
-		'Nayebi te',
-		'Nayebi yo te',
-		'Tomonani Sima',
-		'Zela mukie',
-      ];
+	  
+	  
+const wordList_fula = [
+	["A fami?", ""],
+	["Achu haké hal cess, mi famili", ""],
+	["Jam waali", ""],
+	["Haa gonngol", ""],
+	["No mbadda?", ""],
+	["Mi anda", ""],
+	["Mi fami", ""],
+	["Mi wayri yiide ma", ""],
+	["Holto njeyadha?", ""],
+	["Jaraama", ""],
+	["Laawol bhural", ""],
+	["Nianlen e jam", ""],
+	["Yoo geno reene", ""],
+];
+	  
+const wordList_afrikaans = [
+	["Aangenaame kennis", ""],
+	["Asseblief", ""],
+	["Dankie", ""],
+	["Ek is jammer", ""],
+	["Ek weet nie", ""],
+	["Goed dankie, en met jou?", ""],
+	["Goeie more", ""],
+	["Goeie middag", ""],
+	["Goeie nag", ""],
+	["Ja", ""],
+	["Jy is welkom", ""],
+	["Hallo", ""],
+	["Hoe laat is dit?", ""],
+	["Hoe gaan dit met jou?", ""],
+	["Hoeveel kos dit?", ""],
+	["Nee", ""],
+	["Sien jou later", ""],
+	["Totsiens", ""],
+	["Verskoon my", ""],
+	["Waar is die badkamer", ""],
+	["Wat is jou naam", ""],
+	["Wag 'n bietjie", ""],
+];
+	  
+const wordList_lingala = [
+	["Mbote", "Hello"],
+	["Boni yo, ndeko?", "How are you, brother/sister?"],
+	["Kotuna ye te", "Do not ask him/her"],
+	["Kombo na yo nini?", "What is your name?"],
+	["Lisapo onge!", "Once upon a time!"],
+	["Matondi mingi", "Thank you very much"],
+	["Mbotama elamu", "Happy birthday"],
+	["Mbongo eza wapi?", "Where is the money?"],
+	["Nakomi na tembe", "I have doubts"],
+	["Tosenga ye bolimbisi", "Let's apologize to him/her"],
+	["Nayebi te", "I don't know"],
+	["Nayebi yo te", "I do not know him/her"],
+	["Naza na pongi te", "I am not sleepy"],
+	["Nazali malamo", "I am fine"],
+	["Sambelelaka ba ndeko na yo", "Pray for your family"],
+	["Sango nini?", "What are the news?"],
+	["Soki olingi", "If you want"],
+	["Tomonani sima", "See you later"],
+	["Zela mukie", "Wait a bit"],
+];
+	  
 
       function reinitGame() {
         game = initNewGame();
@@ -112,13 +152,14 @@ function getGameLanguage() {
 
       function gameEndHandler() {
         const content = game.hasWon()
-          ? `Great, you are a winner!`
-          : `Oh no, dear! <br />We were looking for: <br /><em style='text-decoration: underline;'>"${game.getWord()}"</em>.`;
+          ? `Great, you are a winner! <br /><br />You found: <em style='text-decoration: underline;'>"${game.getWord()}"</em>. You seem to know the language; can you add new words to our glossary? Swipe right!`
+          : `Oh no, dear! <br /><br />We were looking for: <em style='text-decoration: underline;'>"${game.getWord()}"</em>. It means: <em style='text-decoration: underline;'>"${game.getTranslatedWord()}"</em>.`;
         showModal(content);
       }
 
       function initNewGame() {
-        const hangman = new Hangman(getRandomWord(), gameEndHandler);
+//        const hangman = new Hangman(getRandomWord(), gameEndHandler);
+		const hangman = new Hangman(getRandomWord(), getTranslationOfChosenWord(), gameEndHandler);
         hideModal();
         drawGame(hangman);
 		/* Test Functions. Keep in comments */
@@ -127,15 +168,32 @@ function getGameLanguage() {
         return hangman;
       }
 
-function getRandomWord() {
+var chosenWordTranslated = "";
+function getRandomWord() { 
+	if(globalGameLanguage == "fula") {
+		const index = Math.floor(Math.random() * wordList_fula.length);
+		
+		chosenWordTranslated = wordList_fula[index][1];
+		
+		return wordList_fula[index][0];
+	}
 	if(globalGameLanguage == "afrikaans") {
 		const index = Math.floor(Math.random() * wordList_afrikaans.length);
-		return wordList_afrikaans[index];
+		
+		chosenWordTranslated = wordList_afrikaans[index][1];
+		
+		return wordList_afrikaans[index][0];
 	}
 	if(globalGameLanguage == "lingala") {
 		const index = Math.floor(Math.random() * wordList_lingala.length);
-		return wordList_lingala[index];
+		
+		chosenWordTranslated = wordList_lingala[index][1];
+		
+		return wordList_lingala[index][0];
 	}
+}
+function getTranslationOfChosenWord() {
+	return chosenWordTranslated;
 }
 
       function guessLetter(letter) {
@@ -228,16 +286,21 @@ function getRandomWord() {
 
       function listenForInputs(callback) {
         window.addEventListener('keydown', event => {
-          const pressedOtherKey = event.altKey || event.ctrlKey || event.metaKey; // Shift key is allowed
+			const excludedKeyCodes = [8, 9, 13, 16, 17, 18, 19, 20, 27, 32, 33, 34, 35, 36, 37, 38, 39, 40, 44, 45, 46];
+			if(excludedKeyCodes.includes(event.keyCode)) {}
+			else {
+          const pressedOtherKey = event.altKey || event.ctrlKey || event.metaKey || event.shiftKey; // Shift key is allowed
           const key = event.key.toUpperCase();
-          const isLetter = key.match(/[A-Z]/);
+          const isLetter = key.match(/[A-Za-z]/g);
           !pressedOtherKey && isLetter && callback(key);
+			}
         });
       }
 
       class Hangman {
-        constructor(word, onEndGame) {
+        constructor(word, translatedWord, onEndGame) {
           this.word = word.toUpperCase();
+		  this.translatedWord = translatedWord;
           this.pickedLetters = [];
           this.faults = 0;
           this.onEndGame = onEndGame;
@@ -246,6 +309,9 @@ function getRandomWord() {
         getWord() {
           return this.word;
         }
+		getTranslatedWord() {
+			return this.translatedWord;
+		}
 
         getCharsOfWord() {
           return this.word.split('');
